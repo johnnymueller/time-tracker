8a957b9135b21f42f17834c2f0f9aec55c2c66b8
John Mueller <jmueller@John-Muellerss-MacBook-Pro.local>
2018-12-10 10:08:05 -0700

    Time incrementing, add to history

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      message: '',
+      taskId: '',
       tasks: [
         {
           id: 1,
@@ -25,32 +26,70 @@ class App extends Component {
         {
           id: 24,
           task_id: 2,
-          duration: '1:15'
+          duration: 4500
         },
         {
           id: 23,
           task_id: 1,
-          duration: '0:15'
+          duration: 900
         },
         {
           id: 22,
           task_id: 74,
-          duration: '0:30'
+          duration: 1800
         },
         {
           id: 21,
           task_id: 2,
-          duration: '3:00'
+          duration: 10800
         },
       ],
       trackingTime: false,
+      intervalId: null,
       currentTime: 0,
     };
   }
 
   toggleTracking = () => {
-    let tracking = this.state.trackingTime;
-    this.setState({trackingTime: !tracking});
+    let tracking = !this.state.trackingTime;
+    this.setState({trackingTime: tracking});
+
+    if (tracking) {
+      let intervalId = setInterval(this.increaseTime, 1000);
+      this.setState({intervalId});
+    } else {
+      clearInterval(this.state.intervalId);
+    }
+  }
+
+  increaseTime = () => {
+    let time = this.state.currentTime;
+    this.setState({currentTime: time + 1});
+  }
+
+  addTime = () => {
+    this.setState({message: ''});
+    if (!this.state.taskId) {
+      this.setState({message: 'You must select a task to add time.'})
+      return;
+    }
+
+    if (this.state.trackingTime) {
+      this.toggleTracking();
+      this.setState({taskId: ''});
+      this.setState({currentTime: 0});
+    }
+
+    let history = this.state.history;
+
+    let historyItem = {
+      id: history.length, // TODO: replace once using real data
+      task_id: parseInt(this.state.taskId, 10),
+      duration: this.state.currentTime
+    };
+
+    history.unshift(historyItem);
+    this.setState({history});
   }
 
   getDuration = (seconds) => {
@@ -60,17 +99,22 @@ class App extends Component {
   }
 
   getTaskDescription = (id) => {
-    let task = this.state.tasks.find((task) => { return task.id == id })
+    let task = this.state.tasks.find((task) => { return task.id === id })
     return task.description
+  }
+
+  handleChange = (event) => {
+    this.setState({taskId: event.target.value});
   }
 
   render() {
     return (
       <div className="App">
-        <select>
+        <div className="message">{this.state.message}</div>
+        <select value={this.state.taskId} onChange={this.handleChange}>
           <option>Select Task</option>
           {this.state.tasks.map(function(task) {
-            return <option key={task.id}>{task.description}</option>
+            return <option key={task.id} value={task.id}>{task.description}</option>
           })}
         </select>
         <br />
@@ -81,10 +125,13 @@ class App extends Component {
             <span>Start</span>
           )}
         </button>
-        <div>{this.getDuration(this.state.currentTime)}</div>
-        <ul>
+        <button onClick={() => this.addTime()}>Add</button>
+        <div>
+          <input type="text" value={this.getDuration(this.state.currentTime)} readOnly />
+        </div>
+        <ul className="history">
           {this.state.history.map((item) => {
-            return <li key={item.id}>{this.getTaskDescription(item.task_id)} - {item.duration}</li>
+            return <li key={item.id}>{this.getTaskDescription(item.task_id)} - {this.getDuration(item.duration)}</li>
           })}
         </ul>
       </div>
