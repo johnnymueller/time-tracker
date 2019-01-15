405b5595695ec84558182c15f7a6202b84fb4e7a
John Mueller <john@cleave.co>
2019-01-15 13:09:29 -0700

    Disable auth

--- api/src/routes/api.php
+++ api/src/routes/api.php
@@ -21,10 +21,10 @@ Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout');
 Route::post('register', 'Auth\RegisterController@register');
 
-Route::group(['middleware' => 'auth:api'], function() {
+// Route::group(['middleware' => 'auth:api'], function() {
     Route::get('articles', 'ArticleController@index');
     Route::get('articles/{article}', 'ArticleController@show');
     Route::post('articles', 'ArticleController@store');
     Route::put('articles/{article}', 'ArticleController@update');
     Route::delete('articles/{article}', 'ArticleController@delete');
-});
+// });
